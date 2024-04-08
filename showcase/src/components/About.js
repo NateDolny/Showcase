5b77cb32adbe44590ceaf38e757757d8a1c36cb1
Nate <nathan.dolny@outlook.com>
2024-04-08 17:21:54 -0600

    created and styled layout for introduction

--- showcase/src/components/About.js
+++ showcase/src/components/About.js
@@ -3,8 +3,26 @@ import React from 'react';
 function About(){
     return (
         <section id="about">
-            <h2>Hi I am Nate</h2>
-            
+            <h2 class="flex justify-center font-bold md:max-w-2xl md:mx-auto 
+            lg:max-w-4xl m-5 text-3xl">Hi I am Nate</h2>
+           
+            <p class="flex justify-center md:max-w-2xl md:mx-auto lg:max-w-4xl 
+            m-5">As a third-year computer science student at the University of 
+            Saskatchewan, my passion for technology has been the driving force
+            behind my creative endeavors. Inspired by the immersive worlds of 
+            science fiction, particularly classics like Dune, I have embarked 
+            on numerous projects fuled by imagination and a thirst for 
+            learning.</p>
+
+            <p class="flex justify-center md:max-w-2xl md:mx-auto lg:max-w-4xl 
+            m-5">I am also deeply immersed in the GNU / Linux ecosystem, 
+            finding joy in its versatility and open-source ethos. Recently, 
+            I have found myself drawn to the intricate realm of DevOps. 
+            Exploring this field has aligned perfectly with my profound 
+            interest in automation, while sparking a keen curiosity about 
+            deploying infastructure. I am eager to apply my education, 
+            personal experiences, and love for GNU / Linux to excel in 
+            this dynamic field.</p>
         </section>
     );
 }
