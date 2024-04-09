579d5cb4c9795b6292e855084f75e7a9da36b728
Nate <nathan.dolny@outlook.com>
2024-04-08 19:44:00 -0600

    touched Certifications.js

---
+++ showcase/src/components/Certifications.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Certifications(){
+    return (
+        <section id="Certifications">
+        <h2>Lets Connect</h2>
+
+        </section>
+    );
+}
+export default Certifications;
