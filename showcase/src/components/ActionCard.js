4ab39a734b020c18fcbd7c6463cd2f5dbb35e3d5
Nate <nathan.dolny@outlook.com>
2024-04-10 20:45:25 -0600

    updated ActionCard component to accept images

--- showcase/src/components/ActionCard.js
+++ showcase/src/components/ActionCard.js
@@ -4,6 +4,9 @@ function Card(props){
     return (
     
 <div class="max-w-md p-8 shadow-sm bg-yellow-300 rounded-xl">
+    <div class="grid place-items-center">
+    <img src={props.img} alt={props.img_description} class="w-12 h-12" />
+    </div>
     <h3 class="p-2 text-center text-lg font-bold">{props.title}</h3>
     <p class="p-2">{props.description}</p>
 </div>
