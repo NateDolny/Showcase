11bc4568cf97739a16ac6d949eb023483fe1019f
Nate <nathan.dolny@outlook.com>
2024-04-10 22:54:29 -0600

    spawn ActionCard components to display methods of contact

--- showcase/src/components/Contact.js
+++ showcase/src/components/Contact.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import ActionCard from './ActionCard';
+import Github from './github.svg';
+import Linkedin from './linkedin-in.svg';
+import Envelope from './envelope-solid.svg';
 
 function Contact(){
     return (
         <section id="Contact">
-        <h2>Lets Connect</h2>
+        <h2 class="text-xl font-bold">Lets Connect</h2>
+       
+        <ActionCard img={Github} img_description='Github logo'/>
 
+        <ActionCard img={Linkedin} img_description='linkedin logo'/>
+        
+        <ActionCard img={Envelope} img_description='Envelope logo'/>
         </section>
     );
 }
