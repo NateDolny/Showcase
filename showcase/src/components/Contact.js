80df75be17e8524b7606641513093e7bbd1a2e7a
Nate <nathan.dolny@outlook.com>
2024-04-10 23:05:56 -0600

    set methods of contact ActionCard components in a grid formation

--- showcase/src/components/Contact.js
+++ showcase/src/components/Contact.js
@@ -7,13 +7,18 @@ import Envelope from './envelope-solid.svg';
 function Contact(){
     return (
         <section id="Contact">
-        <h2 class="text-xl font-bold">Lets Connect</h2>
+ <h2 class="grid place-items-center text-xl font-bold">Lets Connect</h2>
+
+  <div class="grid grid-row-1 gap-4 sm:grid-cols-3 md:max-w-2xl
+        md:mx-auto lg:max-w-4xl m-5">
        
         <ActionCard img={Github} img_description='Github logo'/>
 
         <ActionCard img={Linkedin} img_description='linkedin logo'/>
         
         <ActionCard img={Envelope} img_description='Envelope logo'/>
+        </div>
+
         </section>
     );
 }
