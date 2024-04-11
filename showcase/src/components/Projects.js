95c72e06bb0fa64bcd640334e3d0f09bfd7e6d5b
Nate <nathan.dolny@outlook.com>
2024-04-10 21:16:09 -0600

    set personal projects in a grid formation

--- showcase/src/components/Projects.js
+++ showcase/src/components/Projects.js
@@ -8,6 +8,8 @@ function Projects(){
         <section id="Projects">
             <h2 class="grid place-items-center text-xl font-bold">Projects</h2>
             
+        <div class="grid grid-row-1 gap-4 sm:grid-cols-2 md:max-w-2xl 
+        md:mx-auto lg:max-w-4xl m-5">
             <Card title='barefetch' description='barefetch delivers essential
             system info with minimal clutter, perfect for GNU/Linux users
             seeking streamlined performance.'/>
@@ -22,6 +24,7 @@ function Projects(){
 
             <ActionCard img={GithubIcon} img_description='github icon' 
             title='Explore More'/>
+        </div>
         </section>
     );
 }
