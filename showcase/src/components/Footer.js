3bbcdc098881d55a5d7865fb680d90c769571436
Nate <nathan.dolny@outlook.com>
2024-04-12 16:53:49 -0600

    added sections and section contents to footer component

--- showcase/src/components/Footer.js
+++ showcase/src/components/Footer.js
@@ -3,7 +3,35 @@ import React from 'react';
 function Footer(){
  return (
     <section id="Footer">
+    
+    <div class="bg-stone-200 min-w-full">
+        <div>
+            <p class="font-bold">Nate Dolny</p>
+            <p>Saskatoon,SK</p>
+            <br/>
+            <p>Copyright&copy 2025</p>
+            <p>All Rights Reserved</p>
+        </div>
 
+        <div>
+            <p class="font-bold">Sections</p>
+            <p>About</p>
+            <p>Showcase</p>
+            <p>Contact</p>
+        </div>
+
+        <div>
+            <p class="font-bold">Contact</p>
+            <p>Github</p>
+            <p>Linkedin</p>
+            <p>Email</p>
+        </div>
+
+        <div>
+            <p class="font-bold">Support Me</p>
+            <p>Buy me a coffee</p>
+        </div>
+    </div>
     </section>
     );
 }
