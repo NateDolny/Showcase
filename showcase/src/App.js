c28d0a8d62762c7f62d6d54ec11490ac59111a1a
Nate <nathan.dolny@outlook.com>
2024-04-12 17:47:40 -0600

    created Volunteering component and updated App.js to render component

--- showcase/src/App.js
+++ showcase/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import About from './components/About';
 import Certifications from './components/Certifications';
+import Volunteering from './components/Volunteering';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div class="bg-stone-50 min-h-screen">
+    <div class="bg-stone-100 min-h-screen">
         <About/>
         <Certifications/>
+        <Volunteering/>
         <Projects/>
         <Contact/>
         <Footer/>
