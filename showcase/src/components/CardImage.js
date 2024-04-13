fd647b021dba9ed026b8b549697d1941b2ec704e
Nate <nathan.dolny@outlook.com>
2024-04-13 17:04:23 -0600

    removed shadow effect from CardImage component

--- showcase/src/components/CardImage.js
+++ showcase/src/components/CardImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ActionSocialCard(props){
     return (
     
-<div class="max-w-md shadow-sm rounded-xl">
+<div class="max-w-md  rounded-xl">
     <div class="grid place-items-center">
     <img src={props.img} alt={props.img_description} class="rounded-xl" />
     </div>
