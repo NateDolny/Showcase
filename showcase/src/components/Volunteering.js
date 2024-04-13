8001a1a7813d31b54165a0eb7e189119aceff7f5
Nate <nathan.dolny@outlook.com>
2024-04-12 18:25:10 -0600

    spawn ListCard components to display school organizations

--- showcase/src/components/Volunteering.js
+++ showcase/src/components/Volunteering.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ListCard from './ListCard';
 
 function Volunteering(){
     return (
@@ -6,7 +7,21 @@ function Volunteering(){
         <div class="grid place-items-center m-20">
             <h2 class="font-bold text-xl">Volunteering</h2>
         </div>
-            
+        
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
+        <ListCard title='Computer Science Student Society'
+        subtitle='Office Manager'
+        item1='&#x2022; Responsible for supply of new and old inventory'
+        item2='&#x2022; Responsible for setting markup prices for new inventory'
+        item3='&#x2022; Responsible for managing office hours of operation'
+        item4='&#x2022; Ensured stock inventory and executive tabs kept accurate'/>
+
+        <ListCard title='Access and Equality Services' 
+        subtitle='Notetaker'
+        item1='&#x2022; Created structured notes based on lecture materials'
+        item2='&#x2022; Uploaded notes for students registered with Access and Equality
+        Services'/>
+        </div>
         </section>
     );
 }
