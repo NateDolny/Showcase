538168f93c51d912c10e7e12b4dcb21c0300d29c
Nate <nathan.dolny@outlook.com>
2024-04-13 21:40:12 -0600

    spawn temp ListCard component and spawn ActionCard in Certification component

--- showcase/src/components/Certifications.js
+++ showcase/src/components/Certifications.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import ActionCard from './ActionCard';
+import ListCard from './ListCard';
 
 function Certifications(){
     return (
@@ -6,8 +8,23 @@ function Certifications(){
         <div class="grid place-items-center m-20">
             <h2 class="font-bold text-xl">Certifications</h2>
         </div>
+        <div class="grid grid-cols-1 gap-16 justify-self-start
+            md:grid-cols-2 md:max-w-2xl md:mx-auto lg:grid-cols-2
+            lg:max-w-4xl m-5">
 
-        <div class="p-20 md:p-40 lg:p-40"></div>
+            <div class="max-w-md p-8 shadow-sm bg-stone-200 rounded-xl">
+            <h3 class="text-center text-lg font-bold">Coming Soon!</h3>
+            <div class="p-40"></div>
+            </div>
+
+            <div class="max-w-md p-8 shadow-sm bg-yellow-300 rounded-xl">
+            <div class="p-20"></div>
+            <h3 class="text-center text-lg font-bold">Explore Projects</h3>
+            <div class="p-20"></div>
+            </div>
+            </div>
+
+        <div class="p-20 md:p-32 lg:p-32"></div>
         </section>
     );
 }
