dbe1175647a13862129346fce52978e2bbef164c
Nate <nathan.dolny@outlook.com>
2024-04-15 18:47:11 -0600

    linked sections in Footer component to component sections

--- showcase/src/components/Footer.js
+++ showcase/src/components/Footer.js
@@ -8,22 +8,25 @@ function Footer(){
 
     <div class="grid grid-rows-1 gap-4 md:grid-cols-4 lg:grid-cols-4
      bg-stone-200 min-w-full">
-        <div class="grid m-5 mb-2 md:justify-self-center lg:justify-self-center">
-            <p class="font-bold">Nate Dolny</p>
+        <div class="grid m-5 mb-2 md:justify-self-center 
+        lg:justify-self-center">
+            <p class="font-bold"><a href="#About">Nate Dolny</a></p>
             <p>Saskatoon,SK</p>
             <br/>
             <p>Copyright &#169; 2025</p>
             <p>All Rights Reserved</p>
         </div>
 
-        <div class="grid m-5 mb-2 md:justify-self-center lg:justify-self-center">
+        <div class="grid m-5 mb-2 md:justify-self-center 
+        lg:justify-self-center">
             <p class="font-bold">Sections</p>
-            <p class="m-2 ml-0">About</p>
-            <p class="m-2 ml-0">Showcase</p>
-            <p class="m-2 ml-0">Contact</p>
+            <p class="m-2 ml-0"><a href="#About">About</a></p>
+            <p class="m-2 ml-0"><a href="#Projects">Showcase</a></p>
+            <p class="m-2 ml-0"><a href="#Contact">Contact</a></p>
         </div>
 
-        <div class="grid m-5 mb-2 md:justify-self-center lg:justify-self-center">
+        <div class="grid m-5 mb-2 md:justify-self-center 
+        lg:justify-self-center">
             <p class="font-bold">Contact</p>
             <p class="m-2 ml-0"><a href="https://github.com/NateDolny">
             Github</a></p>
@@ -32,9 +35,12 @@ function Footer(){
             <p class="m-2 ml-0"><a href="mailto:aht648@usask.ca">Email</a></p>
         </div>
 
-        <div class="grid m-5 mb-2 md:justify-self-center lg:justify-self-center">
+        <div class="grid m-5 mb-2 md:justify-self-center 
+        lg:justify-self-center">
             <p class="font-bold">Support Me</p>
-            <p class="m-2 ml-0">Buy me a coffee</p>
+            <p class="m-2 ml-0">
+            <a href="https://www.buymeacoffee.com/natedolny">Buy me a coffee
+            </a></p>
         </div>
     </div>
     </section>
