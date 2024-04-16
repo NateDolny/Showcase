ee45e891149a0be5c335f576f85687b0828ba49c
Nate <nathan.dolny@outlook.com>
2024-04-15 21:48:27 -0600

    updated Card component to accept links

--- showcase/src/components/Card.js
+++ showcase/src/components/Card.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function Card(props){
     return (
-    
 <div class="max-w-md p-8 shadow-sm bg-stone-200 rounded-xl">
+    <a href={props.link}>    
     <h3 class="p-2 text-center text-lg font-bold">{props.title}</h3>
     <p class="p-2">{props.description}</p>
+    </a>
 </div>
     );
 }
