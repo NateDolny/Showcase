30e0be1d0e849284487f0896e0252e3735aa041d
Nate <nathan.dolny@outlook.com>
2024-04-15 18:40:33 -0600

    linked contact links in Footer component to social accounts

--- showcase/src/components/Footer.js
+++ showcase/src/components/Footer.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 function Footer(){
  return (
-    <section id="Footer">
-    
+    <section id="Footer" class="overflow-x-hidden">
+   
+    <div class="p-50"></div>
+
     <div class="grid grid-rows-1 gap-4 md:grid-cols-4 lg:grid-cols-4
      bg-stone-200 min-w-full">
         <div class="grid m-5 mb-2 md:justify-self-center lg:justify-self-center">
@@ -23,9 +25,11 @@ function Footer(){
 
         <div class="grid m-5 mb-2 md:justify-self-center lg:justify-self-center">
             <p class="font-bold">Contact</p>
-            <p class="m-2 ml-0">Github</p>
-            <p class="m-2 ml-0">Linkedin</p>
-            <p class="m-2 ml-0">Email</p>
+            <p class="m-2 ml-0"><a href="https://github.com/NateDolny">
+            Github</a></p>
+            <p class="m-2 ml-0"><a href="https://ca.linkedin.com/in/nate-dolny"
+            >Linkedin</a></p>
+            <p class="m-2 ml-0"><a href="mailto:aht648@usask.ca">Email</a></p>
         </div>
 
         <div class="grid m-5 mb-2 md:justify-self-center lg:justify-self-center">
