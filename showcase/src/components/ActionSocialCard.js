67870f628add8735344618fb07c586511117ec1f
Nate <nathan.dolny@outlook.com>
2024-04-15 21:50:27 -0600

    updated ActionSocialCard component to accpet links

--- showcase/src/components/ActionSocialCard.js
+++ showcase/src/components/ActionSocialCard.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 function ActionSocialCard(props){
     return (
-    
+
+<a href={props.link}>
 <div class="max-w-md p-8 shadow-sm bg-yellow-300 rounded-xl">
     <div class="grid place-items-center p-4">
     <img src={props.img} alt={props.img_description} class="w-12 h-12" />
     </div>
-</div>
+</div></a>
     );
 }
 
